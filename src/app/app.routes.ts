bfc179085870ae733a856ce8d7601c525d028b57
CristianBarrera512 <davidgg98d@gmail.com>
2026-08-12 17:31:00 -0500

    avances con creacion de torneo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CreacionTorneos } from './pages/organizacion/creacion-torneos/creacion-torneos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'torneo',
+    component: CreacionTorneos
+  }
+];
