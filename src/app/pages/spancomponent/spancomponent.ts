6dc589d37a9a04d1e957046843d18ef15b5eddef
BryanAlexander-Reyes <brayanreyes47k@gmail.com>
2026-08-13 14:46:10 -0500

    se genera componente de spanreutilizable y el componente de validador

---
+++ src/app/pages/spancomponent/spancomponent.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spancomponent',
+  imports: [],
+  templateUrl: './spancomponent.html',
+  styleUrl: './spancomponent.css',
+})
+export class SpanComponent {}
